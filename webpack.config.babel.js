bca004779474e88d09c8baec07f3ac67eeb82390
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-08 00:21:16 +0300

    Keep dependencies in a vendor budle

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -28,6 +28,17 @@ const plugins = {
       // add globals' names in .eslintrc to avoid warnings
       IS_DEVELOPMENT: JSON.stringify(isDevelopment),
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: `vendor`,
+      minChunks: (module) => {
+        // This prevents stylesheet resources with the .css or .scss extension
+        // from being moved from their original chunk to the vendor chunk
+        if (module.resource && (/^.*\.(css|scss)$/).test(module.resource)) {
+          return false;
+        }
+        return module.context && module.context.indexOf(`node_modules`) !== -1;
+      },
+    }),
     new HtmlWebpackPlugin({
       filename: `index.html`,
       template: `./src/index.ejs`,
