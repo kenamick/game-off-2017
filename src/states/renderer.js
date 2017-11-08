8d48156b52984284ed5508d6c732a2bae059ead6
carlos.braga <carlos.braga@admcsc.com.br>
2017-11-08 17:05:35 -0200

    Change method of stretching the game screen
    This avoids the drawback of losing Phaser pointer
    tracking precision and lets Phaser take care of
    the rendering work (including which renderer will
    be used).

--- src/states/renderer.js
+++ src/states/renderer.js
@@ -1,14 +1,5 @@
 // renderer.js - Base class.
-// Scales the game screen and copies the scaled buffer to the canvas.
 // Each state needs to extend from the Renderer.
-
-const pixel = { 
-  scale: 4, 
-  canvas: null, 
-  context: null, 
-  width: 0, 
-  height: 0 
-};
 
 let fps;
 
@@ -19,49 +10,26 @@ class Renderer {
   }
 
   /**
-   * Scales game screen. 
-   * 
+   * Scales game screen.
+   *
    * Renders to a backbuffer and copies to main canvas. This needs to be called
    * only once when initializing the game.
-   * 
+   *
    * @see http://www.photonstorm.com/phaser/pixel-perfect-scaling-a-phaser-game
    */
-  initOnce() {
-    //  Hide the un-scaled game canvas
-    this.game.canvas.style['display'] = 'none';
-    
-    //  Create our scaled canvas. It will be the size of the game * whatever scale value you've set
-    pixel.canvas = Phaser.Canvas.create(this.game.width * pixel.scale, 
-      this.game.height * pixel.scale);
-    
-      //  Store a reference to the Canvas Context
-    pixel.context = pixel.canvas.getContext('2d');
-    
-    //  Add the scaled canvas to the DOM
-    Phaser.Canvas.addToDOM(pixel.canvas);
-    
-    //  Disable smoothing on the scaled canvas
-    Phaser.Canvas.setSmoothingEnabled(pixel.context, false);
+  init() {
+    // scale the game 2x (make game 480x320 wide, still 3:2 ratio)
+    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    this.game.scale.setUserScale(2, 2);
 
-    //  Cache the width/height to avoid looking it up every render
-    pixel.width = pixel.canvas.width;
-    pixel.height = pixel.canvas.height;
+    // enable crisp rendering
+    this.game.renderer.renderSession.roundPixels = true;
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
   }
 
   enableFps() {
     let font = { font: '11px Arial', fill: 'white' };
     fps = this.add.text(5, 5, '', font);
-  }
-
-  /**
-   * Renders the unscaled canvas to the displayed canvas.
-   * 
-   * Needs to be called from every state in order to update the game contents
-   * on screen.
-   */
-  render() {
-    pixel.context.drawImage(this.game.canvas, 0, 0, this.game.width, this.game.height, 
-      0, 0, pixel.width, pixel.height);
   }
 
   update() {
