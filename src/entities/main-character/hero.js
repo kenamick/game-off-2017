09edbb20764e45b301579a3160a3f692eeebeb57
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-09 12:23:17 +0300

    Add controls getters, add xinput support

--- src/entities/main-character/hero.js
+++ src/entities/main-character/hero.js
@@ -11,30 +11,94 @@ class Hero {
   }
 
   spawn(x, y) {
+    const game = this.game;
     this._sprite = this.game.add.sprite(x, y, 'atlas_sprites', 'hero_stand_01');
-    
-    // sets anchor in the middle of the sprite, so that we can flip it 
+
+    // sets anchor in the middle of the sprite, so that we can flip it
     // when moving left/right
     this._sprite.anchor.set(0.5, 0.5);
 
     // bind animation frames (check the json file for details/adjustments)
-    this._sprite.animations.add('stand', 
+    this._sprite.animations.add('stand',
       Phaser.Animation.generateFrameNames('hero_stand_', 1, 3, '', 2), 5, true);
-    this._sprite.animations.add('walk', 
+    this._sprite.animations.add('walk',
       Phaser.Animation.generateFrameNames('hero_walk_', 1, 6, '', 2), 10, true);
-    this._sprite.animations.add('combo', 
+    this._sprite.animations.add('combo',
       Phaser.Animation.generateFrameNames('hero_combo_', 1, 6, '', 2), 10, true);
-    this._sprite.animations.add('jump', 
+    this._sprite.animations.add('jump',
       Phaser.Animation.generateFrameNames('hero_jump_', 1, 3, '', 2), 10, true);
-    this._sprite.animations.add('airkick', 
+    this._sprite.animations.add('airkick',
       Phaser.Animation.generateFrameNames('hero_airkick_', 1, 1, '', 2), 10, true);
 
     this._sprite.animations.play('stand');
 
-    this.game.physics.arcade.enable(this._sprite);
+    game.physics.arcade.enable(this._sprite);
 
     // camera always follows the main player
-    this.game.camera.follow(this._sprite);    
+    game.camera.follow(this._sprite);
+
+    // game.input.addKeys API is not sufficient since
+    // character needs to be aslo controlled by gamepad and AI
+
+    game.input.gamepad.start()
+    const pad1 = game.input.gamepad.pad1
+    const stickThreshold = 0.1;
+
+    // TODO: Get controls scheme from separatre config
+    // Maybe move to a separate class
+
+    this.controls = {
+      get up () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.W) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.UP) || (
+            pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) ||
+            pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -stickThreshold
+          )
+        );
+      },
+      get down () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.S) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.DOWN)|| (
+            pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) ||
+            pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > stickThreshold
+          )
+        );
+      },
+      get left () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.A) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || (
+            pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) ||
+            pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -stickThreshold
+          )
+        );
+      },
+      get right () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.D) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) || (
+            pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) ||
+            pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > stickThreshold
+          )
+        );
+      },
+      get punch () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.H) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.ENTER) ||
+          pad1.isDown(Phaser.Gamepad.XBOX360_X)
+        );
+      },
+      get jump () {
+        return (
+          game.input.keyboard.isDown(Phaser.Keyboard.J) ||
+          game.input.keyboard.isDown(Phaser.Keyboard.SPACE) ||
+          pad1.isDown(Phaser.Gamepad.XBOX360_A)
+        );
+      },
+    }
   }
 
   get sprite() {
@@ -43,11 +107,13 @@ class Hero {
 
   update() {
     const game = this.game;
-    
+
     let moving = false;
     let combo = false;
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+    // console.log(this.controls.punch, this.controls.up)
+
+    if (this.controls.punch) {
       combo = true;
 
       // TODO: Pressing the fight key once needs to play the first
@@ -58,19 +124,19 @@ class Hero {
       // The idea here is not to be able to move while fighting
       // Player has to attack from standing position.
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+      if (this.controls.up) {
         moving = true;
         this._sprite.body.velocity.y = -HeroConsts.SPEED;
         this._sprite.animations.play('walk');
       }
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-        moving = true;      
+      if (this.controls.down) {
+        moving = true;
         this._sprite.body.velocity.y = HeroConsts.SPEED;
         this._sprite.animations.play('walk');
       }
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+      if (this.controls.left && !this.controls.right) {
         moving = true;
 
         this._sprite.body.velocity.x = -HeroConsts.SPEED;
@@ -78,7 +144,7 @@ class Hero {
         this._sprite.animations.play('walk');
       }
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+      if (this.controls.right && !this.controls.left) {
         moving = true;
 
         this._sprite.body.velocity.x = HeroConsts.SPEED;
