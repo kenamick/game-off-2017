0d9568372f7479f7372f0b8d2ff4c65839c60635
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-11 11:53:34 +0300

    Fix localStrorage boolean check

--- src/globals.js
+++ src/globals.js
@@ -6,11 +6,16 @@ const URLOptions = window.location.search.slice(1).split('&')
     return { [keyValue[0]]: [keyValue[1]] }
   });
 
+const checkLocalStorageBoolean =
+  (item) => localStorage.getItem(item) === `true`;
+
 const Globals = {
   // development
-  debug: localStorage.getItem(`debug`),
-  debugPhysics: localStorage.getItem(`debugPhysics`),
-  showFPS: localStorage.getItem(`showFPS`),
+  debug: checkLocalStorageBoolean(`debug`),
+  debugPhysics: checkLocalStorageBoolean(`debugPhysics`),
+  showFPS: checkLocalStorageBoolean(`showFPS`),
+  // maybe add dev-* key to items' names so we can cycle throug
+  // list and check values automatically
   ...URLOptions, // url options override localSotrage values
 }
 
