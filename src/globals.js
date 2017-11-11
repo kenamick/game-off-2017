f73af0e461db92ab5979c78d8c370f6e80817d55
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-11 11:38:19 +0300

    Set globals via localStorage or url options

--- src/globals.js
+++ src/globals.js
@@ -1,11 +1,17 @@
 // globa.js - global game switches and consts
 
+const URLOptions = window.location.search.slice(1).split('&')
+  .map((option) => {
+    const keyValue = option.split('=');
+    return { [keyValue[0]]: [keyValue[1]] }
+  });
+
 const Globals = {
   // development
-  debug: true,
-  debugPhysics: true,
-  showFPS: true
-
+  debug: localStorage.getItem(`debug`),
+  debugPhysics: localStorage.getItem(`debugPhysics`),
+  showFPS: localStorage.getItem(`showFPS`),
+  ...URLOptions, // url options override localSotrage values
 }
 
 export default Globals;
