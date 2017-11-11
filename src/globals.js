0f1896a392856a9a229c172c54d5d22b5cf122c8
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-11 12:22:23 +0300

    Fix URLOptions and simplify bool check

--- src/globals.js
+++ src/globals.js
@@ -1,19 +1,18 @@
 // globa.js - global game switches and consts
 
-const URLOptions = window.location.search.slice(1).split('&')
-  .map((option) => {
-    const keyValue = option.split('=');
-    return { [keyValue[0]]: [keyValue[1]] }
-  });
+const checkStringBoolean = (item) => item === `true`;
 
-const checkLocalStorageBoolean =
-  (item) => localStorage.getItem(item) === `true`;
+const URLOptions = {};
+window.location.search.slice(1).split('&').map((option) => {
+  const keyValue = option.split('=');
+  URLOptions[keyValue[0]] = checkStringBoolean(keyValue[1]) || keyValue[1];
+});
 
 const Globals = {
   // development
-  debug: checkLocalStorageBoolean(`debug`),
-  debugPhysics: checkLocalStorageBoolean(`debugPhysics`),
-  showFPS: checkLocalStorageBoolean(`showFPS`),
+  debug: checkStringBoolean(localStorage.getItem(`debug`)),
+  debugPhysics: checkStringBoolean(localStorage.getItem(`debugPhysics`)),
+  showFPS: checkStringBoolean(localStorage.getItem(`showFPS`)),
   // maybe add dev-* key to items' names so we can cycle throug
   // list and check values automatically
   ...URLOptions, // url options override localSotrage values
