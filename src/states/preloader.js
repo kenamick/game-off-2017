91db7da2589f2855bbfba9dbae2eca77c64a0bf4
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-11-12 22:33:02 +0100

    Add Act 2 (still a draft) and a test transition
    Go to the end of Act1 level to load the Act2 level.

--- src/states/preloader.js
+++ src/states/preloader.js
@@ -14,10 +14,14 @@ class Preloader extends Renderer {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
 
     // load levels
+    this.game.load.image('gd-tiles', require('../assets/levels/gd-tileset.png'));
+
     this.game.load.tilemap('act1', 
       require('file-loader!../assets/levels/act1.json'), null,
       Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('gd-tiles', require('../assets/levels/gd-tileset.png'));
+    this.game.load.tilemap('act2', 
+      require('file-loader!../assets/levels/act2.json'), null,
+      Phaser.Tilemap.TILED_JSON);
 
     // load sprites atlas
     this.load.atlas('atlas_sprites',
@@ -30,7 +34,7 @@ class Preloader extends Renderer {
   update() {
     if (this._ready) {
       // TODO: change this to menu in production
-      this.state.start('play');
+      this.state.start('act1');
     }
   }
 
