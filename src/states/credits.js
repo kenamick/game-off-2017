28412f9797afc1ec7bbcd6cf9e34a5feece1dab8
Alexey Vishnyakov <n3tn0de@gmail.com>
2017-11-30 02:06:05 +0300

    Update credits and refactore them a little

--- src/states/credits.js
+++ src/states/credits.js
@@ -10,28 +10,36 @@ class Credits extends Renderer {
   create() {
     super.create();
 
-    const creditsTitle = this.game.add.bitmapText(this.game.world.centerX, 24, Globals.bitmapFont, 'CREDITS', 24);
+    const creditsTitle = this.addText(
+      'CREDITS',
+      this.game.world.centerX, 24, 24
+    );
+
     creditsTitle.anchor.setTo(0.5);
 
-    this.game.add.bitmapText(5, 50, Globals.bitmapFont, 'Producer:', 10);
-    this.game.add.bitmapText(10, 65, Globals.bitmapFont, 'Petar', 12);
+    const columnsTop = 45;
 
-    this.game.add.bitmapText(5, 85, Globals.bitmapFont, 'Audio Designer:', 10);
-    this.game.add.bitmapText(10, 100, Globals.bitmapFont, 'LampEight', 12);
+    const column1Left = 5;
+    const column1Padding = column1Left + 5;
 
-    this.game.add.bitmapText(
-      this.game.world.width / 2 - 5, 50,
-      Globals.bitmapFont, 'Visuals:', 10);
-    this.game.add.bitmapText(
-      this.game.world.width / 2, 65,
-      Globals.bitmapFont, 'Petar, PsyBack', 12);
+    const column2Left = this.game.world.width / 2 - 5;
+    const column2Padding = this.game.world.width / 2;
 
-    this.game.add.bitmapText(
-      this.game.world.width / 2 - 5, 85,
-      Globals.bitmapFont, 'Programmers:', 10);
-    this.game.add.bitmapText(
-      this.game.world.width / 2, 100,
-      Globals.bitmapFont, `Alexey, Carlos, \nPetar`, 12);
+    this.addText('Producer:', column1Left, columnsTop);
+    this.addText('Petar Petrov', column1Padding, columnsTop + 15);
+
+    this.addText('Audio Designer:', column1Left, columnsTop + 45);
+    this.addText('Will Tonna', column1Padding, columnsTop + 60);
+
+    this.addText('Visuals:', column2Left, columnsTop);
+    this.addText(
+      'Petar Petrovsss\nPavel Antonov',
+      column2Padding, columnsTop + 15);
+
+    this.addText('Programmers:', column2Left, columnsTop + 45);
+    this.addText(
+      `Alexey Vishnyakov\nCarlos Braga\nPetar Petrov`,
+      column2Padding, columnsTop + 60);
 
     // leave text
     const leaveText = this.game.add.bitmapText(this.game.world.centerX, 155, Globals.bitmapFont, '(Press Punch key to continue)', 8);
@@ -41,6 +49,16 @@ class Credits extends Renderer {
     this.game.add.tween(leaveText).to({ alpha: 1   }, 1000, Phaser.Easing.Linear.None, true, 0, -1, true);
 
     this.controls = new Controls(this.game, true);
+  }
+
+  addText(
+    text='text',
+    x=0,
+    y=0,
+    fontSize=10,
+    font=Globals.bitmapFont,
+  ) {
+    return this.game.add.bitmapText(x, y, font, text, fontSize);
   }
 
   update() {
