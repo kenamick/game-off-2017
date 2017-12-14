1df84788fb797e5eabaef3bb44e7f966747f5481
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-12-14 21:58:34 +0100

    Minor changes in weather
     - removed unused code
     - tried to make rain fixed to camera and lower the particles count
       so to lower the CPU usage, but it didn't work so far

--- src/specialfx/weather.js
+++ src/specialfx/weather.js
@@ -1,4 +1,4 @@
-// textdraw.js - text drawing routines and fx
+// weather.js - raining & weather fx
 
 import Globals from '../globals';
 
@@ -10,11 +10,12 @@ class Weather {
 
   addRain() {
     const raindropSprite = this.game.add.graphics();
-    raindropSprite.beginFill(Phaser.Color.hexToRGB(Globals.palette.sky.hex),  1)
+    raindropSprite.beginFill(Phaser.Color.hexToRGB(Globals.palette.sky.hex), 1)
              .drawRect(0, 0, 6, 20)
              .endFill();
 
     const raindropTexture = raindropSprite.generateTexture();
+
     this.emitter = this.game.add.emitter(this.game.world.centerX, -500, 1600);
     this.emitter.width = this.game.world.width;
     this.emitter.angle = 12;
@@ -31,41 +32,8 @@ class Weather {
     this.emitter.maxRotation = 0;
 
     this.emitter.flow(2000, 5, 5, -1);
-  }
 
-  /**
-   * Displays a blinking hand at the given x,y location
-   */
-  hand(x, y, orientation = 'down') {
-    const hand = this.game.add.sprite(x, y, 'atlas_sprites', 'hand');
-    hand.alpha = 1;
-    // hand.scale.x = -0.9;
-    // hand.scale.y = -0.9;
-
-    switch (orientation) {
-      case 'down':
-        hand.scale.y = -hand.scale.y;
-      break;
-      case 'right':
-        hand.anchor.setTo(0.5);
-        hand.angle += 90;
-      break;
-      // more ...?
-    }
-
-    const toY = hand.y + 15;
-
-    const tween = this.game.add.tween(hand).to({ alpha: .25, y: toY }, 
-      800, Phaser.Easing.Linear.None , true, 0, -1, true);
-
-    // tween.onLoop.add(() => {
-    //   console.log('text')
-    //   if (this.audio) {
-    //     this.audio.play(this.audio.sfx.go);
-    //   }
-    // });
-
-    return hand;
+    //this.emitter.fixedToCamera = true;
   }
 
 }
