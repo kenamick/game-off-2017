5f2e6d619796663f1d8c3fda5136aadd019f21bb
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-12-19 21:10:39 +0100

    Add stopRain() method to be able to toggle rain fx

--- src/specialfx/weather.js
+++ src/specialfx/weather.js
@@ -9,29 +9,40 @@ class Weather {
   }
 
   addRain() {
-    const raindropSprite = this.game.add.graphics();
-    raindropSprite.beginFill(Phaser.Color.hexToRGB(Globals.palette.sky.hex), 1)
-             .drawRect(0, 0, 4, 45)
-             .endFill();
+    if (this.emitter) {
+      // this.emitter.revive();
+      this.emitter.flow(1100, 5, 5, -1);
+    } else {
+      const raindropSprite = this.game.add.graphics();
+      raindropSprite.beginFill(Phaser.Color.hexToRGB(Globals.palette.sky.hex), 1)
+              .drawRect(0, 0, 4, 45)
+              .endFill();
 
-    const raindropTexture = raindropSprite.generateTexture();
+      const raindropTexture = raindropSprite.generateTexture();
 
-    this.emitter = this.game.add.emitter(this.game.world.centerX, -500, 1600);
-    this.emitter.width = this.game.world.width;
-    this.emitter.angle = 12;
+      this.emitter = this.game.add.emitter(this.game.world.centerX, -500, 1600);
+      this.emitter.width = this.game.world.width;
+      this.emitter.angle = 12;
 
-    this.emitter.makeParticles(raindropTexture);
+      this.emitter.makeParticles(raindropTexture);
 
-    this.emitter.minParticleScale = 0.1;
-    this.emitter.maxParticleScale = 0.2;
+      this.emitter.minParticleScale = 0.1;
+      this.emitter.maxParticleScale = 0.2;
 
-    this.emitter.setYSpeed(300, 600);
-    this.emitter.setXSpeed(-5, 5);
+      this.emitter.setYSpeed(300, 600);
+      this.emitter.setXSpeed(-5, 5);
 
-    this.emitter.minRotation = 0;
-    this.emitter.maxRotation = 0;
+      this.emitter.minRotation = 0;
+      this.emitter.maxRotation = 0;
 
-    this.emitter.flow(1100, 5, 5, -1);
+      this.emitter.flow(1100, 5, 5, -1);
+    }
+  }
+
+  stopRain() {
+    if (this.emitter) {
+      this.emitter.kill();
+    }
   }
 
 }
